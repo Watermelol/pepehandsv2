1f987f95e635d79c9371e988b0ad73eb39b83a00
bobbytee369 <bobbytee725@gmail.com>
2021-01-04 18:02:19 +0800

    Redirect user to questionaire page if they didnt answer the questionaire b4 and update admin page

--- Maia/models.py
+++ Maia/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+
+class user_profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    questionaire_answered = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile.objects.create(user=instance)
+    instance.user_profile.save()
--- Maia/views.py
+++ Maia/views.py
@@ -2,12 +2,23 @@ from django.shortcuts import render
 from django.http import Http404
 from django.contrib.auth import logout
 from django.shortcuts import redirect
+from .models import user_profile
 
 # Create your views here.
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    if request.user.is_authenticated:
+        # check if the user got answer the questionair before or not
+        current_user = user_profile.objects.get(user_id=request.user.id)
+        questionaire_answered = getattr(current_user, 'questionaire_answered')
+        # if user answer b4 then redirect him to dashboard else bring him to questionaire page
+        if (questionaire_answered == True):
+            return render(request, 'dashboard.html')
+        else:
+            return redirect('/questionaire')
+    
 
 def login(request):
+    # check if the user is log in ady or not
     if request.user.is_authenticated:
         return redirect('/dashboard')
     else:
